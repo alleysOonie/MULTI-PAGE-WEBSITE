578a0864d431ec2cc9ad5639386b33b0eca456e3
alleysOonie <nicholeallyson77@gmail.com>
2026-05-23 12:04:38 +0800

    web-app

---
+++ multi-page-web/src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <nav className="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
