e8d8c6e765da99ffc36aa718b844863223cbebdc
alleysOonie <nicholeallyson77@gmail.com>
2026-05-23 12:10:53 +0800

    pages

--- multi-page-web/src/pages/About.js
+++ multi-page-web/src/pages/About.js
@@ -1,7 +1,7 @@
 function About() {
   return (
     <div>
-      <h1>About</h1>
+      <h1>About Page</h1>
       <p>This is the About page.</p>
     </div>
   );
--- multi-page-web/src/pages/Home.js
+++ multi-page-web/src/pages/Home.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Home Page</h1>
       <p>Welcome to the Home page.</p>
     </div>
   );
